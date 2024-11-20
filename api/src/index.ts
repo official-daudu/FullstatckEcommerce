639133a2c7ec4a5f58c1a0b5fe95cbebe3db3201
psammiekeys <psammiekeys@gmail.com>
2024-11-20 15:25:22 +0100

    product router setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,10 @@
 import express from "express";
+import productsRouter from "./routes/products/index";
 
-const port = 3000;
+const port = 3030;
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World! 123");
-});
+app.use("/products", productsRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
